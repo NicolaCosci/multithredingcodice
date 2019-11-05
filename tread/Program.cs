b3d8438d4f9f2dd9db1a28d5f295c0df13b0dce6
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 08:23:21 +0100

    Aggiungere i file di progetto.

---
+++ tread/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace tread
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Thread mythread = new Thread(() =>
+             {
+                 Console.WriteLine("MyThread è iniziato");
+                 Thread.Sleep(1000);
+                 Console.WriteLine("MyThread è terminato");
+
+             });
+
+            //Esecuzione myThread
+            mythread.Start();
+
+            Thread.Sleep(500);
+            Console.WriteLine("Main Thread");
+            string someVariabile = "Matteo Tumiati";
+
+            var workerThread = new Thread((o) =>
+            {
+                Console.WriteLine($"Saluti da: {someVariabile}");
+
+            });
+
+            workerThread.Start();
+            someVariabile = "Daniele Bocchicchio";
+
+
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
