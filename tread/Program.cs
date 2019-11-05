e274592fa71ddcf78e7061f6522f61f9e36b71d8
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-11-05 09:50:29 +0100

    secondo commento

--- tread/Program.cs
+++ tread/Program.cs
@@ -9,6 +9,9 @@ namespace tread
 {
     class Program
     {
+        private static object index;
+        private static object thread;
+
         public static void Main(string[] args)
         {
             Thread mythread = new Thread(() =>
@@ -35,9 +38,61 @@ namespace tread
             workerThread.Start();
             someVariabile = "Daniele Bocchicchio";
 
+            var list = new List<Thread>();
+            //qui creiamo ed eseguiamo cinque worker thread for (int index=0; index <5; index++)
 
+            {
+                var myThread = new Thread((currentIndex) =>
+                {
+                    Console.WriteLine("Thread {0} è iniziato", currentIndex);
+                    Thread.Sleep(500);
+                    Console.WriteLine("Thread {0} è terminato", currentIndex);
+                });
+                myThread.Start(index);
+                list.Add(myThread);
+            }
+            //Attesa del completamento di ognuno dei worker thread 
+            foreach (Thread thread in list)
+           
+            {
+                thread.Join();
+            }
+            Console.WriteLine("Esecuzione di tutti i thread terminata");
+            //Attesa di massimo un secondo thread.join(1000);
+            Thread workerThread = new Thread(() =>
+            {
+                Console.WriteLine("inizio di un thread molto lungo");
+                Thread.Sleep(5000);
+                Console.WriteLine("Termine workwe thread");
 
+            });
+            workerThread.Start();
+            workerThread.Join(500);
 
+            //se i worker thread è ancora in esecuzione lo si cancella 
+            if (workerThread.ThreadState != ThreadState.Stopped)
+            {
+                workerThread.Abort();
+            }
+            Console.WriteLine("Termine applicazione");
+
+            var workerThread = new Thread(() =>
+            {
+                try
+                {
+                    Console.WriteLine("Inizio di un thread molto lungo");
+                    Thread.Sleep(5000);
+                    Console.WriteLine("Termine worker thread ");
+
+                }
+                catch (ThreadAbortException ex)
+                {
+                    //qui codice per gestire l'eccezione
+                }
+
+            });
+
+            workerThread.IsBackground = false; workerThread.Priority = ThreadPriority.Lowest;
             Console.ReadLine();
         }
         
